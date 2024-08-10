254f831db10cafb4c11f3538d7360ef10cc8cdfc
rohangargcse <rohancool24012003@gmail.com>
2024-08-10 20:11:04 +0530

    js files changed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-  const apiKey = "YOUR API KEY";
+  const apiKey = "API KEY";
   const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
   const searchBox = document.querySelector(".search input");
   const searchBtn = document.querySelector(".search button");
